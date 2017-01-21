bfb33e0fad71a9c32363bed1ba4a937cb03d4014
S1erp1nsk1 <02autumn.guffey@gulllakecs.org>
2017-01-21 17:47:02 -0500

    Test Push 5:45

---
+++ SteamworksProductTest/src/Robot.cpp
@@ -0,0 +1,23 @@
+#include <WPILib.h>
+#include "RobotMap.h"
+
+using namespace frc;
+class Robot: public IterativeRobot
+{
+	RobotDrive myRobot { 0, 1 };  // robot drive system
+	Joystick stick { 0 };  // only joystick
+
+public:
+	Robot() {
+		myRobot.SetExpiration(0.1);
+	}
+
+	void TeleopPeriodic()
+	{
+			myRobot.ArcadeDrive(stick); // drive with arcade style (use right stick)
+		// wait for a motor update time
+
+	}
+};
+
+START_ROBOT_CLASS(Robot)
