ba7aeef5b59f07e8bdd286fef511f1ce35588309
S1erp1nsk1 <02autumn.guffey@gulllakecs.org>
2017-01-21 20:43:10 -0500

    adding a file

---
+++ SteamworksProductTest/src/RobotMap.h
@@ -0,0 +1,21 @@
+/*
+ * RobotMap.h
+ *
+ *  Created on: Jan 21, 2017
+ *      Author: auffe_000
+ */
+
+#ifndef SRC_ROBOTMAP_H_
+#define SRC_ROBOTMAP_H_
+
+const static int frontRightMotor = 0;
+const static int frontLeftMotor = 1;
+const static int backRightMotor = 2;
+const static int backLeftMotor = 3;
+
+const static int light1Input = 9;
+const static int ultraSensorOutput = 0;
+
+
+
+#endif /* SRC_ROBOTMAP_H_ */
